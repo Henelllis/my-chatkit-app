f2cac7d5b9a9fe34563009c07206d72ecc57bd17
Henelllis@hellis1001@gmail.com <hellis1001@gmail.com>
2018-07-09 14:37:24 -0400

    Weird issues

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, {state}from 'react'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
+
+class App extends React.Component {
+
+    state = {
+        messages:[],
+    }
+    
+    render() {
+        return (
+            <div className="app">
+                <RoomList />
+                <MessageList />
+                <SendMessageForm />
+                <NewRoomForm />
+            </div>
+        );
+    }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "output": {
+    "filename": "[name].pack.js"
+  },
+  "module": {
+    "rules": [
+      {
+        "use": {
+          "loader": "babel-loader",
+          "options": {
+            "presets": [
+              "babel-preset-env",
+              "babel-preset-react",
+              'react',
+              'stage-2'
+            ]
+          }
+        },
+        "exclude": /node_modules/,
+        "test": /\.js$/
+      }
+    ]
+  },
+  "entry": {
+    "index": "./index"
+  }
+};
