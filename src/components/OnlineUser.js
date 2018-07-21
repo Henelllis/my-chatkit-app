2cf89e8f5735f86043406e83ba717464616a7023
Henelllis@hellis1001@gmail.com <hellis1001@gmail.com>
2018-07-21 12:54:36 -0400

    Online Users feature

--- src/components/OnlineUser.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const onlineUser = props => {
-    return null;
-}
-
-export default onlineUser;
--- src/components/OnlineUsersList.js
+++ src/components/OnlineUsersList.js
@@ -1,14 +1,37 @@
 import React , { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { connect } from 'react-redux';
+import Avatar from '@material-ui/core/Avatar';
+import pink from '@material-ui/core/colors/pink';
 
 import OnlineUser from './OnlineUser';
 
-class OnlineUsersList extends Component{
+const styles = {
+    row: {
+    //   display: 'inline',
+      justifyContent: 'center',
+    },
+    avatar: {
+      margin: 10,
+    },
+    pinkAvatar: {
+        margin: 10,
+        color: '#fff',
+        backgroundColor: pink[500],
+      },
+  };
 
+class OnlineUsersList extends Component{
+   
     shouldComponentUpdate(nextProps, nextState){
        return nextProps.onlineUsers !== this.props.onlineUsers ;
     };
 
     render() {
+
+        const {classes} = this.props;
+
         if(this.props.onlineUsers.length === 0){
             return(
                 <div className='online-users-list'>
@@ -16,24 +39,38 @@ class OnlineUsersList extends Component{
                 </div>
             )
         }
-    
+        
         return(
             <div className='online-users-list'>
                 <ul className='online-users-list'>
                     <h3> Online Buddies</h3>
-                    {[...this.props.onlineUsers].map(
-                        user => {
-                            return (
-                                <li key={user} className='user'>
-                                    <a>{user}</a>
-                                </li>
-                            )
-                        }
-                    )}
+                    {[...this.props.onlineUsers].filter( user => this.props.user.name !== user)
+                        .map(
+                            user => {
+                                return (
+                                    <li key={user} className='user'>
+                                        <div className={classes.row}>
+                                            <a><Avatar className={classes.pinkAvatar}>{user.charAt(0)}</Avatar><p>{user}</p></a>
+                                        </div>
+                                    </li>
+                                )
+                            }
+                        )}
                 </ul>
             </div>
         );
     }
 };
 
-export default OnlineUsersList;
+OnlineUsersList.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+
+  const mapStateToProps = state => {
+    return{
+        user: state.login.currentUser,
+    }
+}
+
+
+export default connect(mapStateToProps)(withStyles(styles)(OnlineUsersList));
